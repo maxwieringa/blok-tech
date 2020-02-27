c4520052ad38db81a775bc402cbd3b6b9cf1d8d4
maxwieringa <maxwieringa01@gmail.com>
2020-02-27 11:38:17 +0100

    Github repo verbeterd zodat alle code meteen te zien is.

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/user', (req, res) => res.send('bruhmoment'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
