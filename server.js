1fd32f8470648b85372f382a1346074d8fea1a88
maxwieringa <maxwieringa01@gmail.com>
2020-02-28 14:05:18 +0100

    Added gitignore file and ejs template engine

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/user', (req, res) => res.send('bruhmoment'))
