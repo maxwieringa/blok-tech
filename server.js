380a4cf7a5f0d54c758cb706d86c8aa6288ef8fe
maxwieringa <maxwieringa01@gmail.com>
2020-03-16 21:54:37 +0100

    Updated jobstory

--- server.js
+++ server.js
@@ -6,51 +6,107 @@ const mongo = require('mongodb');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const mongodbUri = 'mongodb+srv://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@bloktech-r4i80.azure.mongodb.net/bamboos-data?retryWrites=true&w=majority';
+const ejsLint = require('ejs-lint');
+const Schema = mongoose.Schema;
+const db = mongoose.connection;
+
+app.set('view engine', 'ejs');
+app.set('views', 'view');
+app.use(express.static('public'));
+app.get('/', matches);
+app.get('/:id', profile);
+app.get('/add', form);
+app.use(notFound)
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 mongoose.connect(mongodbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
-mongoose.connection.on('connected', () => {
+db.on('connected', () => {
   console.log('Mongoose connected');
 });
 
-//Schema
-const Schema = mongoose.Schema;
-const BlogPostSchema = new Schema({
-  title: String,
-  body: String,
-  date: {
-    type: String,
-    default: Date.now()
+function matches(req, res, next) {
+  db.collection('profiles').find().toArray(done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.render('matches.ejs', {
+        data: data
+      })
+    }
   }
+}
+
+function profile(req, res, next) {
+  var id = req.params.id
+
+  db.collection('profiles').findOne({
+    _id: new mongo.ObjectID(id)
+  }, done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.render('detail.ejs', {
+        data: data
+      })
+    }
+  }
+}
+
+function form(req, res) {
+  res.render('add.ejs')
+}
+
+function add(req, res, next) {
+  db.collection('profiles').insertOne({
+    naam: req.body.naam,
+    leeftijd: req.body.leeftijd,
+    bio: req.body.bio
+  }, done)
+
+  function done(err, data) {
+    if (err) {
+      next(err)
+    } else {
+      res.redirect('/' + data.insertedId)
+    }
+  }
+}
+
+//Schema profielen
+const ProfielSchema = new Schema({
+  naam: String,
+  leeftijd: Number,
+  bio: String
 });
 
 //Model
-const BlogPost = mongoose.model('BlogPost', BlogPostSchema);
+const Profiel = mongoose.model('Profile', ProfielSchema);
 
 //Saving data to mongo database
-/* const data = {
-  title: 'Bruhmoment',
-  body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+const profielData = {
+  naam: 'Bruh',
+  leeftijd: 22,
+  bio: 'Ik ben Bruh en ik hou van de office en avonturen.'
 };
 
-const newBlogPost = new BlogPost(data);
+const newProfiel = new Profiel(profielData);
 
-newBlogPost.save((error) => {
+newProfiel.save((error) => {
   if (error) {
-    console.log('Error newBlogPost');
+    console.log('Error newProfiel');
   } else {
     console.log('Data has been saved');
   }
-}); */
+});
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/about', (req, res) => res.send('bruhmoment'));
-app.get('/contact', (req, res) => res.send('Bruh'));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function notFound(req, res) {
+  res.status(404).render('not-found.ejs')
+}
