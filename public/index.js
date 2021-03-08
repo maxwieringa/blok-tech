5a7b3c1654be754f8979d67a25c64239a5f03841
maxwieringa <maxwieringa01@gmail.com>
2021-03-08 14:10:48 +0100

    var naar const veranderd

--- public/index.js
+++ public/index.js
@@ -1,12 +1,12 @@
-var remove = document.getElementById('js-remove')
+const remove = document.getElementById('js-remove')
 
 if (remove) {
   remove.addEventListener('click', onremove)
 }
 
 function onremove(ev) {
-  var node = ev.target
-  var id = node.dataset.id
+  const node = ev.target
+  const id = node.dataset.id
 
   fetch('/' + id, {
       method: 'delete'
